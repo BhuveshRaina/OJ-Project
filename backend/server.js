4d9f2c43aa446c852742309766a8436940ec4ca4
BhuveshRaina <imt_2022031@iiitm.ac.in>
2025-06-14 17:21:02 +0530

    everthing fine

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const boilerplateRoutes = require('./routes/boilerplateRoutes');
 
-// const submissionRoutes = require('./routes/submissionRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-// app.use('/api/submissions', submissionRoutes);
+app.use(bodyParser.urlencoded({ extended: true }));
+// Routes
+app.use('/boilerplate', boilerplateRoutes);
 
 app.get('/', (req, res) => {
   res.send('Online Judge Backend is running');
